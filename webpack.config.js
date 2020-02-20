494c286b7d61b31f74273d23cea5a34295900911
Allison Bradley <allison.bradley@outlook.com>
2020-02-19 19:13:24 -0700

    Added bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,14 @@
-const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
-const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 
 const config = {
   mode: "development",
-  entry: "./public/index.js",
+  entry: {
+    app: "./public/index.js",
+    db: "./public/db.js"
+  },
   output: {
     path: __dirname + "/public/dist",
-    filename: "bundle.js"
+    filename: "[name].bundle.js"
   },
-  plugins: [
-    new SWPrecacheWebpackPlugin({
-      cacheId: "my-domain-cache-id",
-      dontCacheBustUrlsMatching: /\.\w{8}\./,
-      filename: "service-worker.js",
-      minify: true,
-      staticFileGlobsIgnorePatterns: [/\.map$/, /manifest\.json$/]
-    }),
-    new WebpackPwaManifest({
-      name: "Budget Tracker",
-      filename: "manifest.json",
-      inject: "false",
-      short_name: "Budget",
-      description: "An application to track your expenditures and income",
-      background_color: "#01579b",
-      theme_color: "#ffffff",
-      "theme-color": "#ffffff",
-      start_url: "/",
-    })
-  ]
 };
 module.exports = config;
